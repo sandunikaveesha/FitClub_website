3639eaa2860fdb42adce2497ed13b55f17bf565a
SanduniWimalasiri <it20022792@my.sliit.lk>
2022-10-31 15:47:47 +0530

    add-bar

--- fitclub/src/components/Hero/Hero.jsx
+++ fitclub/src/components/Hero/Hero.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Hero.css'
+import Header from '../Header/Header'
 
 const Hero = () =>{
     return (
         <div className="hero">
-            
+            <div className="left-h">
+                <Header/>
+
+                <div className="the-best-add">
+                    <div></div>
+                    <span>The best fitness club in the city</span>
+                </div>
+            </div>
+            <div className="right-h">The right side</div>
         </div>
     )
 }
