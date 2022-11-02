4b340cb0c0874f8e0327949359991d92bf5547c0
SanduniWimalasiri <it20022792@my.sliit.lk>
2022-11-02 14:46:17 +0530

    hero_section_heading

--- fitclub/src/components/Hero/Hero.jsx
+++ fitclub/src/components/Hero/Hero.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
-import Header from '../Header/Header'
 import './Hero.css'
+import Header from '../Header/Header'
 
 const Hero = () =>{
     return (
         <div className="hero">
-           <div className="left-h">
+            <div className="left-h">
                 <Header/>
+{/*the best add*/}
+                <div className="the-best-add">
+                    <div></div>
+                    <span>The best fitness club in the city</span>
+                </div>
 
-                
-           </div>
-           <div className="right-h">right side</div> 
+{/*heading of the hero section*/}
+            <div className="hero-text">
+                <div>
+                    <span className='stroke-text'>Shape</span>
+                    <span>Your</span>
+                </div>
+                <div>
+                    <span>Ideal Body</span>
+                </div>
+                <div>
+                    <span>In here we will help you to shape and build your ideal body and live up your
+                            life to fullest
+                    </span>
+                </div>
+            </div>
+            </div>
+            <div className="right-h">The right side</div>
         </div>
     )
 }
